f739e34f2094951298c86e6a45d3ca77a5f2ca57
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-30 23:06:32 +1300

    Created simple welcome component

---
+++ frontend/src/Components/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+    return (
+        <div className="flex justify-center">
+            <h1 className="text-4xl">Welcome!</h1>
+        </div>
+    );
+}
+ 
+export default Welcome
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../Components/Header";
+import Welcome from "../Components/Welcome";
 
 const Home = () => {
     return (
         <div className="">
             <Header/>
+            <Welcome/>
         </div>
     );
 }

d091103ae31c8f1f0d0683a05cfee2d5176bc052
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-30 23:02:52 +1300

    Fixed tailwind and created simple header

---
+++ frontend/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex justify-end p-4">
+            <button className="border border-black px-4 rounded">Login</button>
+        </header>
+    );
+}
+ 
+export default Header;

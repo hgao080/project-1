d69e0c900a85d52a2c3504e3c3a2358e97f6b271
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-01 12:18:39 +1300

    Integrated creation of events

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,21 @@ app.get('/api/events', (req, res) => {
     Event.find({}).then(events => {
         res.json(events)
     })
-}) 
+})
+
+app.post('/api/events', (req, res) => {
+    const body = req.body
+
+    const event = new Event({
+        name: body.name,
+        description: body.description,
+        date: body.date,
+    })
+
+    event.save().then(savedEvent => {
+        res.json(savedEvent)
+    })
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

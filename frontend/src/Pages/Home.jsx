afeb7b6a9b5e306982196e2142d8d471f5203caa
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-30 23:53:05 +1300

    Made simple create account button

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,13 +3,15 @@ import Welcome from "../components/Welcome";
 import Events from "../components/Events";
 
 const Home = () => {
-    
 
     return (
         <div className="m-auto max-w-[50rem]">
             <Header/>
             <Welcome/>
             <Events/>
+            <div className="flex justify-center mt-[3rem]">
+                <button className="border border-black px-4 rounded">Create account</button>
+            </div>
         </div>
     );
 }

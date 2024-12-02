dbb77d819d50489f0db9c9ad06a402d2f0ec6bd9
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-03 00:45:19 +1300

    Frontend now reflecting if event is joined or not

--- frontend/src/Components/Event.jsx
+++ frontend/src/Components/Event.jsx
@@ -1,34 +1,42 @@
-import eventServices from '../services/events'
+import { useEffect, useState } from "react";
 
-const Event = ({ data, user }) => {
-    
+import eventServices from "../services/events";
+
+const Event = ({ event, user }) => {
+  const [isJoined, setIsJoined] = useState(false);
+
+  useEffect(() => {
+    if (event.users.includes(user.username)) {
+        setIsJoined(true)
+    }
+  }, []);
+
   const handleJoin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const joiningUser = {
-        username: user.username
-    }
+      username: user.username,
+    };
 
-    eventServices
-        .joinEvent(data.id, joiningUser)
-        .then((event) => {
-            console.log(event)
-        })
+    eventServices.joinEvent(event.id, joiningUser).then((event) => {
+      setIsJoined(true)
+    });
   };
 
   return (
     <div className="flex border border-black w-[30rem] px-2 py-1 justify-between items-center">
       <div className="flex flex-col">
-        <h3 className="font-bold">{data.name}</h3>
-        <p className="italic mt-[-4px]">{data.date}</p>
-        <p>{data.description}</p>
+        <h3 className="font-bold">{event.name}</h3>
+        <p className="italic mt-[-4px]">{event.date}</p>
+        <p>{event.description}</p>
       </div>
       {user ? (
         <button
           onClick={handleJoin}
           className="border border-black px-2 rounded"
+          disabled={isJoined}
         >
-          Join
+          {isJoined ? 'Joined' : 'Join'}
         </button>
       ) : (
         ""
--- frontend/src/Components/Events.jsx
+++ frontend/src/Components/Events.jsx
@@ -6,7 +6,7 @@ const Events = ({ events, user }) => {
             <h2 className='text-3xl underline'>Events</h2>
             <div className="flex flex-col gap-2 mt-[1rem]">
             {events.map(event => (
-                <Event key={event.id} data={event} user={user}/>
+                <Event key={event.id} event={event} user={user}/>
             ))}
             </div>
         </div>

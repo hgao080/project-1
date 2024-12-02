8f9623218f3f5062f866f18e6377e861e05e4155
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-02 15:47:31 +1300

    Installed JWT to authenticate users on sign up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
+require('dotenv').config()
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.SECRET, { expiresIn: '3d'})
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -12,7 +18,9 @@ const signUpUser = async (req, res) => {
     try {
         const user = await User.signUp(email, password, username)
 
-        res.status(200).json(user)
+        const token = createToken(user.id)
+
+        res.status(200).json(token)
     } catch (err) {
         res.status(400).json({error: err.message})
     }

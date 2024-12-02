37a62e77b81a901147bde4c69baff7b8f6552a08
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-02 23:12:34 +1300

    Setup login and logout functionality

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,8 +1,18 @@
 import { NavLink } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Header = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
+
     return (
         <header className="flex justify-end p-4">
+            <div>
+                <button onClick={handleClick}>Log out</button>
+            </div>
             <NavLink to="login" className="border border-black px-4 rounded">Log In</NavLink>
         </header>
     );
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {login, error, isLoading} = useLogin()
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email, password);
+        await login(email, password)
     }
 
     return (
@@ -20,7 +22,8 @@ const Login = () => {
             <label htmlFor="">Password:</label>
             <input type="password" onChange={(e) => setPassword(e.target.value)}/>
 
-            <button>Log in</button>
+            <button disabled={isLoading}>Log in</button>
+            {error && <div>{error}</div>}
         </form>
     );
 }

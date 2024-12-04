5ec170627924cc37726991ab67cdb247d57c280b
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-04 20:54:32 +1300

    Fixed frontend routes being handled by backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 const url = process.env.MONGODB_URI;
@@ -9,12 +10,16 @@ const url = process.env.MONGODB_URI;
 const eventRoutes = require('./routes/events')
 const userRoutes = require('./routes/user')
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
 
 app.use('/api/events', eventRoutes)
 app.use('/api/user', userRoutes)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)

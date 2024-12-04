da9bce93c0846279892a1a36e37388096b0e9a87
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-04 18:14:42 +1300

    Styling of signup page

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -26,15 +26,15 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex h-screen w-screen">
-      <div className="flex items-center m-auto border border-black rounded-lg">
+    <div className="flex h-screen w-screen bg-homeBg bg-no-repeat bg-center bg-cover font-main">
+      <div className="flex items-center m-auto border border-black rounded-lg bg-beige">
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col m-auto px-4 rounded-xl py-2 items-center w-[40rem]"
+          className="flex flex-col m-auto px-4 rounded-xl py-20 items-center w-[40rem]"
         >
-          <h3 className="m-auto text-3xl underline">Sign up</h3>
+          <h3 className="m-auto text-6xl underline font-bold decoration-2 underline-offset-2">Sign up</h3>
 
-          <div className="flex mt-4 w-[60%] m-auto gap-4 translate-x-[-2.5rem]">
+          <div className="flex mt-4 w-[60%] m-auto gap-4 translate-x-[-2.5rem] font-bold tracking-wide text-2xl">
             <div className="flex flex-col gap-2 items-end">
               <label htmlFor="email">Email</label>
               <label htmlFor="password">Password</label>
@@ -65,22 +65,22 @@ const SignUp = () => {
             </div>
           </div>
 
-          <div className="border border-yellow-400 px-4 py-2 mt-4 text-yellow-600 rounded">
-            <p className="">Password must contain at least:</p>
+          <div className="border border-black px-4 py-2 mt-4 rounded text-xl bg-golden-yellow shadow-xl">
+            <p className="font-bold">Password must contain at least:</p>
             <ul className="grid grid-cols-2 gap-x-4 list-disc ml-4">
                 <li>One capital letter</li>
                 <li>One lowercase letter</li>
-                <li>A number</li>
+                <li>One number</li>
             </ul>
           </div>
 
           <button
             disabled={isLoading}
-            className="border border-green-600 mt-4 px-4 rounded text-green-600"
+            className="block border border-black bg-pastel-green mt-4 px-4 rounded transition-all shadow-lg text-xl hover:translate-y-[-2px] active:translate-y-[2px]"
           >
             Sign up
           </button>
-          {error && <div className="border border-red-500 text-red-500 px-4 mt-4 rounded">{error}</div>}
+          {error && <div className="border border-red-500 text-red-500 px-4 mt-4 rounded text-xl font-bold">{error}</div>}
         </form>
       </div>
     </div>

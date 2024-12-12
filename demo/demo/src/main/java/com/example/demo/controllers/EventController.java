d2a2c1425db700f59c6864f7f5800db022126ea6
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-12 14:53:42 +1300

    Basic delete endpoint

--- demo/demo/src/main/java/com/example/demo/controllers/EventController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/EventController.java
@@ -3,10 +3,13 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Event;
@@ -28,6 +31,12 @@ public class EventController {
     @PostMapping
     public ResponseEntity<Object> createEvent(@RequestBody Event eventData) {
         eventRepository.save(eventData);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteEvent(@PathVariable("id") String id) {
+        eventRepository.deleteById(id);
         return ResponseEntity.ok().build();
     }
 }

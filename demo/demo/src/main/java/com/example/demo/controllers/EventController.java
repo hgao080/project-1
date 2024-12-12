73c166eb2a7b0f5819b0d1a69ff5b9675a8c3b08
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-13 11:38:39 +1300

    Add competition to event endpoint

--- demo/demo/src/main/java/com/example/demo/controllers/EventController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/EventController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controllers;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +41,17 @@ public class EventController {
     public ResponseEntity<Object> deleteEvent(@PathVariable("id") String id) {
         eventRepository.deleteById(id);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> addCompetition(@PathVariable("id") String id, @RequestBody Map<String, Object> data) {
+        Optional<Event> optionalEvent = eventRepository.findById(id);
+        Event existingEvent = optionalEvent.get();
+
+        String competitionId = (String) data.get("competitionId");
+        existingEvent.setCompetitionId(competitionId);
+        eventRepository.save(existingEvent);
+
+        return ResponseEntity.ok(existingEvent);
     }
 }

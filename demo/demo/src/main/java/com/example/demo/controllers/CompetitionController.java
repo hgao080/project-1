468e0aef5b7ed05d0ff81676ef608fc98d0a741f
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-12-13 11:24:40 +1300

    Get all competitions endpoint

--- demo/demo/src/main/java/com/example/demo/controllers/CompetitionController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/CompetitionController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -39,4 +40,8 @@ public class CompetitionController {
         return ResponseEntity.ok(comp);
     }
 
+    @GetMapping
+    public ResponseEntity<Object> getCompetitions() {
+        return ResponseEntity.ok(competitionRepository.findAll());
+    }
 }

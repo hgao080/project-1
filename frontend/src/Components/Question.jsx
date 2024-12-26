84a85810a2d6712471f90b19d40200b1546337b7
Henry Gao <henrygao48@gmail.com>
2024-12-26 15:31:42 +1300

    Really simply frontend for when a user is doing a competition test (No error handling whatsoever)

--- frontend/src/Components/Question.jsx
+++ frontend/src/Components/Question.jsx
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AnswersContext } from "../Pages/CompetitionTest";
 
-const Question = ({question}) => {
-    const [selectedChoice, setSelectedChoice] = useState(null);
+const Question = ({ question }) => {
+  const {answers, setAnswers} = useContext(AnswersContext)
 
-    return (
-        <div className="">
-            <h2>{question.title}</h2>
-            <div className="grid grid-cols-2">
-                    {question.options.map((option, index) => (
-                        <div key={index}>
-                        <label>
-                          <input
-                            type="radio"
-                            value={index}
-                            onChange={(e) => setSelectedChoice(e.target.value)}
-                            checked={selectedChoice === index.toString()}
-                          />
-                          {option}
-                        </label>
-                      </div>
-                    ))}
-            </div>
-        </div>
-    );
+  const handleSelectChoice = (questionTitle, index) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionTitle]: index,
+    }))
+  }
+
+  return (
+    <div className="w-full">
+      <h2 className="">{question.title}</h2>
+      <div className="grid grid-cols-2">
+        {question.options.map((option, index) => (
+          <div key={index} className="flex gap-2 items-center">
+            <input
+              type="radio"
+              value={index}
+              id={`${question.title}-${index}`}
+              onChange={() => handleSelectChoice(question.title, index)}
+              checked={answers[question.title] === index}
+              className=""
+            />
+            <label htmlFor={`${question.title}-${index}`} className="">
+              {option}
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
- 
+
 export default Question;
--- frontend/src/Pages/CompetitionTest.jsx
+++ frontend/src/Pages/CompetitionTest.jsx
@@ -1,13 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, createContext } from "react";
 import { useParams } from "react-router-dom";
 
 import competitionsService from "../services/competitions"
 import Question from "../components/Question";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+export const AnswersContext = createContext();
 
 const CompetitionTest = () => {
     const { competitionId } = useParams()
+    const { user } = useAuthContext()
 
     const [questions, setQuestions] = useState([]);
+    const [answers, setAnswers] = useState({})
 
     useEffect(() => {
         competitionsService.getQuestions(competitionId).then((data) => {
@@ -15,16 +20,29 @@ const CompetitionTest = () => {
         })
     }, []);
 
+    const handleSubmit = () => {
+        const AttemptObject = {
+            userEmail: user.email,
+            competitionId,
+            attempts: answers
+        }
+
+        console.log(AttemptObject)
+    }
+
     return (
-        <div className="">
-            <h1 className="">{competitionId}</h1>
-            <div className="flex flex-col w-full items-center">
-                {questions.map((question) => (
-                    <Question key={question.title} question={question}/>
-                ))}
+        <div className="flex flex-col">
+            <h1 className="m-auto mt-12">{competitionId}</h1>
+            <div className="grid grid-cols-2 mt-6 w-[60rem] m-auto">
+                <AnswersContext.Provider value={{ answers, setAnswers }}>
+                    {questions.map((question) => (
+                        <Question key={question.title} question={question} />
+                    ))}
+                </AnswersContext.Provider>
             </div>
+            <button className="mt-6" onClick={handleSubmit}>Submit</button>
         </div>
     );
 }
- 
+
 export default CompetitionTest;

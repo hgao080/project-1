574c43a8f0eb4e876c02ebf33654cc97abcae992
Henry Gao <henrygao48@gmail.com>
2025-01-10 19:40:25 +1300

    Fixed changing username to an already existing username

--- demo/demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -132,6 +133,13 @@ public class UserController {
         User user = userRepository.findByUsername(username);
 
         if (data.containsKey("username")) {
+            User existingUser = userRepository.findByUsername(data.get("username"));
+            if (existingUser != null) {
+                Map<String, String> error = new HashMap<>();
+                error.put("error", "Username already taken");
+                return ResponseEntity.badRequest().body(error);
+            }
+
             user.setUsername(data.get("username"));
         } else {
             user.addJoinedEvents(data.get("eventName"));

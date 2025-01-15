34f6f3a0a9f32fa585e3709130aa67ecb6cc83c0
Henry Gao <henrygao48@gmail.com>
2025-01-15 13:39:55 +1300

    Ensure all fields for event creation are not empty

--- frontend/src/Components/EventForm.jsx
+++ frontend/src/Components/EventForm.jsx
@@ -1,73 +1,101 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import eventsService from "../services/events";
+import eventsService from '../services/events';
 
 const EventForm = ({ events, setEvents }) => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState("");
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
+	const [date, setDate] = useState('');
+	const [error, setError] = useState('');
 
-  const createEvent = (e) => {
-    e.preventDefault();
+	const createEvent = (e) => {
+		e.preventDefault();
 
-    const eventObject = {
-      name: name,
-      description: description,
-      date: date,
-    };
+		if (!name || !description || !date) {
+			setError('All fields are required');
+			return;
+		}
 
-    eventsService.createEvent(eventObject).then((createdEvent) => {
-      setEvents(events.concat(createdEvent));
-      setName("");
-      setDescription("");
-      setDate("");
-    });
-  };
+		const eventObject = {
+			name: name,
+			description: description,
+			date: date,
+		};
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-  const handleDescriptionChange = (e) => {
-    setDescription(e.target.value);
-  };
-  const handleDateChange = (e) => {
-    setDate(e.target.value);
-  };
+		eventsService.createEvent(eventObject).then((createdEvent) => {
+			setEvents(events.concat(createdEvent));
+			setName('');
+			setDescription('');
+			setDate('');
+			setError('');
+		});
+	};
 
-  return (
-    <div className="flex flex-col mt-[1.28rem] gap-2">
-      <h3 className="text-4xl underline text-center text-nowrap decoration-2 tracking-wide text-warm-brown font-bold">Create an event</h3>
-      <form
-        onSubmit={createEvent}
-        className="border border-black rounded-xl p-2 px-4 w-[15rem] shadow-2xl bg-beige font-body"
-      >
-        <div className="">
-          <p className="underline font-bold text-xl tracking-wide">Name</p>
-          <input
-            type="text"
-            onChange={handleNameChange}
-            className="w-full border border-black rounded pl-1 text-xl tracking-wide"
-            value={name}
-          />
-        </div>
-        <div className="mt-1">
-          <p className="underline font-bold text-xl tracking-wide">Description</p>
-          <textarea
-            onChange={handleDescriptionChange}
-            className="border rounded border-black pl-1 w-full h-[10rem] resize-none text-xl"
-            value={description}
-          />
-        </div>
-        <div className="mt-0">
-            <p className="underline font-bold text-xl tracking-wide">Date</p>
-            <input type="date" onChange={handleDateChange} className="border border-black rounded pl-1 w-full text-xl tracking-wide" value={date}/>
-        </div>
-        <div>
-          <button type="submit" className="border border-black text-black px-4 mt-4 rounded bg-pastel-green transition-all hover:translate-y-[-2px] active:translate-y-[2px] font-bold text-xl">Create</button>
-        </div>
-      </form>
-    </div>
-  );
+	const handleNameChange = (e) => {
+		setName(e.target.value);
+	};
+	const handleDescriptionChange = (e) => {
+		setDescription(e.target.value);
+	};
+	const handleDateChange = (e) => {
+		setDate(e.target.value);
+	};
+
+	return (
+		<div className='flex flex-col mt-[1.28rem] gap-2'>
+			<h3 className='text-4xl underline text-center text-nowrap decoration-2 tracking-wide text-warm-brown font-bold'>
+				Create an event
+			</h3>
+			<form
+				onSubmit={createEvent}
+				className='border border-black rounded-xl p-2 px-4 w-[15rem] shadow-2xl bg-beige font-body'>
+				<div className=''>
+					<p className='underline font-bold text-xl tracking-wide'>
+						Name
+					</p>
+					<input
+						type='text'
+						onChange={handleNameChange}
+						className='w-full border border-black rounded pl-1 text-xl tracking-wide'
+						value={name}
+					/>
+				</div>
+				<div className='mt-1'>
+					<p className='underline font-bold text-xl tracking-wide'>
+						Description
+					</p>
+					<textarea
+						onChange={handleDescriptionChange}
+						className='border rounded border-black pl-1 w-full h-[10rem] resize-none text-xl'
+						value={description}
+					/>
+				</div>
+				<div className='mt-0'>
+					<p className='underline font-bold text-xl tracking-wide'>
+						Date
+					</p>
+					<input
+						type='date'
+						onChange={handleDateChange}
+						className='border border-black rounded pl-1 w-full text-xl tracking-wide'
+						value={date}
+					/>
+				</div>
+				<div className='flex justify-between items-center mt-4'>
+					<button
+						type='submit'
+						className='border border-black text-black px-4 rounded bg-pastel-green transition-all hover:translate-y-[-2px] active:translate-y-[2px] font-bold text-xl'>
+						Create
+					</button>
+					{error && (
+						<div className=' text-red-500 border border-red-500 text-center px-1 rounded-md text-sm'>
+							{error}
+						</div>
+					)}
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default EventForm;

5424e96fe16adecfeb41821697a9d2d01b3fa6cd
Henry Gao <henrygao48@gmail.com>
2025-01-16 12:47:39 +1300

    Update proxy to use env if present

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.PORT || 'http://localhost:8080',
+        target: process.env.VITE_API_URL || 'http://localhost:8080',
         changeOrigin: true,
       },
     }
